4a8f216d6ccb42aba2def3942848257f17e3d209
Yuriy Stul <yuriy.stul@williamhill.com>
2016-03-14 10:54:31 +0200

    playing with debug

---
+++ modules/playWithDebug/useDebugLog.js
@@ -0,0 +1,9 @@
+// Palying with debug logging
+// Use command set DEBUG=useDebugLog
+'use strict';
+
+var debug = require('debug')('useDebugLog');
+
+console.log('it is the log output');
+
+debug('it is debug output')
