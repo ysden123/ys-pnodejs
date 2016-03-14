d19774dbdb551fb3086f5ee02f96fdab3227bfef
Yuriy Stul <yuriy.stul@williamhill.com>
2016-03-14 18:01:53 +0200

    multi_api - continue

---
+++ modules/multi_api/api1.js
@@ -0,0 +1,7 @@
+// API method 1
+
+'use strict';
+
+function f1(d1) {
+  console.log('f1: d1=', d1);
+}
---
+++ modules/multi_api/app_multi_api.js
@@ -0,0 +1,7 @@
+// Main for testing multiple API use.
+
+'use strict';
+
+var api1 = require('api1.js');
+
+api1.f1('the test');
