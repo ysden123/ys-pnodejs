656a1aa38e042c28b4fc7c88085bb2041e06bf87
ysden123 <ysden123@gmail.com>
2016-03-15 23:07:54 +0200

    multiple lines

--- modules/server/postSoap.js
+++ modules/server/postSoap.js
@@ -1,60 +1,67 @@
 // Sends POST request with SOAP
 
+'use strict';
+
 var https = require('https');
 var qs = require('querystring');
 
 function postWithPostData() {
     console.log('==> postWithPostData');
-    var postData = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v9="https://bingads.microsoft.com/Reporting/v9">
-        < soapenv:Header>
-            <v9:UserName>?</v9:UserName>
-                <v9:Password>?</v9:Password>
-                    <v9:DeveloperToken>?</v9:DeveloperToken>
-                        <v9:CustomerId>?</v9:CustomerId>
-                            <v9:CustomerAccountId>?</v9:CustomerAccountId>
-                                <v9:AuthenticationToken>?</v9:AuthenticationToken>
-                                    <v9:ApplicationToken>?</v9:ApplicationToken>
-   </soapenv: Header >
-        <soapenv:Body>
-            <v9:SubmitGenerateReportRequest>
-            <!--Optional: -->
-            <v9:ReportRequest>
-            <!--Optional: -->
-            <v9:Format>?</v9:Format >
-            <!--Optional:-->
-        <v9:Language>?</v9:Language >
-            <!--Optional:-->
-        <v9:ReportName>?</v9:ReportName >
-            <!--Optional:-->
-        <v9:ReturnOnlyCompleteData>?</v9:ReturnOnlyCompleteData >
-         </v9: ReportRequest >
-      </v9: SubmitGenerateReportRequest >
-   </soapenv: Body >
-</soapenv: Envelope > ';
+    
+    var postData =
+        '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v9="https://bingads.microsoft.com/Reporting/v9"> \
+        < soapenv:Header> \
+            <v9:UserName>?</v9:UserName> \
+            <v9:Password>?</v9:Password> \
+            <v9:DeveloperToken>?</v9:DeveloperToken> \
+            <v9:CustomerId>?</v9:CustomerId> \
+            <v9:CustomerAccountId>?</v9:CustomerAccountId> \
+            <v9:AuthenticationToken>?</v9:AuthenticationToken> \
+            <v9:ApplicationToken>?</v9:ApplicationToken> \
+        </soapenv:Header> \
+        <soapenv:Body> \
+            <v9:SubmitGenerateReportRequest> \
+                <!--Optional: --> \
+                <v9:ReportRequest> \
+                    <!--Optional: --> \
+                    <v9:Format>?</v9:Format > \
+                    <!--Optional:--> \
+                    <v9:Language>?</v9:Language > \
+                    <!--Optional:--> \
+                    <v9:ReportName>?</v9:ReportName > \
+                    <!--Optional:--> \
+                    <v9:ReturnOnlyCompleteData>?</v9:ReturnOnlyCompleteData > \
+                </v9: ReportRequest > \
+            </v9: SubmitGenerateReportRequest > \
+       </soapenv: Body > \
+    </soapenv: Envelope > ';
+    
     var options = {
-        hostname: 'https://reporting.api.bingads.microsoft.com/Api/Advertiser/Reporting/V9/ReportingService.svc',
+        hostname: 'reporting.api.bingads.microsoft.com',
         port: 443,
-        path: '/',
+        path: '/Api/Advertiser/Reporting/V9/ReportingService.svc',
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/xml',
+            'Content-Type': 'text/xml',
             'Content-Length': Buffer.byteLength(postData)
         }
     };
 
     var req = https.request(options, (res) => {
         console.log(`STATUS: ${res.statusCode}`);
-        console.log('headers: ${qs.stringify(res.headers)}');
+        // console.log('headers: ${qs.stringify(res.headers)}');
 
         res.setEncoding('utf8');
 
+        let response = "";
         res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
+            //console.log(`BODY: ${chunk}`);
+            response += chunk;
         });
 
         res.on('end', () => {
-            console.log('No more data in response.');
+            console.log('response:');
+            console.log(response);
         });
     });
 
@@ -65,3 +72,5 @@ function postWithPostData() {
     req.write(postData);
     req.end();
 }
+console.info('Getting...');
+postWithPostData();
