9036ba2f75500b4e2f0a6338dd44256d66952091
ysden123 <ysden123@gmail.com>
2016-04-08 20:59:28 +0300

    shift right

---
+++ modules/operators/shifts.js
@@ -0,0 +1,14 @@
+// Playing with shift operators
+'use strict';
+
+console.log(' 1 >> 1 = %d', 1 >> 1);
+console.log(' 3 >> 1 = %d', 3 >> 1);
+console.log(' 3 >> 2 = %d', 3 >> 2);
+console.log(' 10 >> 1 = %d', 10 >> 1);
+console.log(' 10 >> 2 = %d', 10 >> 2);
+
+console.log(' 1 >>> 1 = %d', 1 >>> 1);
+console.log(' 3 >>> 1 = %d', 3 >>> 1);
+console.log(' 3 >>> 2 = %d', 3 >>> 2);
+console.log(' 10 >>> 1 = %d', 10 >>> 1);
+console.log(' 10 >>> 2 = %d', 10 >>> 2);
