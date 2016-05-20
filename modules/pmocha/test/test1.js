e3840ca51cbded015e452775ed3dc39eb40db15c
ysden123 <ysden123@gmail.com>
2016-05-20 20:53:07 +0300

    checking reporters

--- modules/pmocha/test/test1.js
+++ modules/pmocha/test/test1.js
@@ -1,10 +1,14 @@
 'use strict';
 const should = require('chai').should();
+const pmocha = require('..');
 
-describe('test1',function(){
-    describe('foo1',function(){
-        it('Should work proprly',function(){
+describe('test1', function () {
+    describe('foo1', function () {
+        it('Should work properly', function () {
             // Everything is OK
-        })
+        });
+        it('should throw exception', function () {
+            (function () { pmocha.foo1(); }).should.throw(Error);
+        });
     })
 })
