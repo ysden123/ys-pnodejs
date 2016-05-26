3345ef882bc6fbe9f646e6b34846b897c3e4066c
Yuriy Stul <yuriy.stul@williamhill.com>
2016-05-26 08:52:46 +0300

    sorting for string values

--- modules/pjson/sorting.js
+++ modules/pjson/sorting.js
@@ -13,10 +13,33 @@ const o1 = {
     ]
 }
 
-console.log('original:', o1.objs);
+console.log('(o1) original:', o1.objs);
 
 o1.objs.sort((a,b)=>a.key > b.key);
 console.log('sorted (asc):', o1.objs);
 
 o1.objs.sort((a,b)=>a.key < b.key);
 console.log('sorted (desc):', o1.objs);
+
+const o2 = {
+    objs: [
+        { key: "1 abc", val: 1 },
+        { key: "5 test 5", val: 5 },
+        { key: "3 stam", val: 3 },
+        { key: "6 66", val: 6 },
+        { key: "2 two", val: 21 },
+        { key: "2 two", val: 22 },
+        { key: "2 tow", val: 23 },
+        { key: "7 s", val: 7 }
+    ]
+}
+
+
+console.log();
+console.log('(o2) original:', o2.objs);
+
+o2.objs.sort((a,b)=>a.key > b.key);
+console.log('sorted (asc):', o2.objs);
+
+o2.objs.sort((a,b)=>a.key < b.key);
+console.log('sorted (desc):', o2.objs);
