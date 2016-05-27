bad38df3d219e04d8b475b82ec53533ebe3d51d1
ysden123 <ysden123@gmail.com>
2016-05-27 16:15:53 +0300

    simple object

--- modules/priak/test/connect.test.js
+++ modules/priak/test/connect.test.js
@@ -3,9 +3,9 @@ const should = require('chai').should();
 const conf = require('../conf/config').conf;
 const nodiak = require('nodiak');
 
-describe('connection', function () {
+describe('connection', function() {
     this.timeout(1000 * 60 * 3);
-    it('Should work', function (done) {
+    it('Should work', function(done) {
         let riak = nodiak.getClient(conf.riak.backend, conf.riak.host, conf.riak.port);
         riak.ping((err, res) => {
             if (err) {
