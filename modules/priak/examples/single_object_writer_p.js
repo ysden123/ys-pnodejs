cbc02822f809bbb73be935342b8b475b600a3629
ysden123 <ysden123@gmail.com>
2016-05-28 15:29:48 +0300

    multiple objects with promise

--- modules/priak/examples/single_object_writer_p.js
+++ modules/priak/examples/single_object_writer_p.js
@@ -3,7 +3,7 @@
 
 const conf = require('../conf/config').conf;
 const manager = require('simple-node-logger').createLogManager();
-const logger = manager.createLogger('[connects]');
+const logger = manager.createLogger('[single_object_writer_p]');
 
 const Client = require('riaks');
 const client = new Client({
@@ -16,7 +16,6 @@ client.connect()
     .then(() => logger.info('Connected'))
     .catch((err) => logger.error(err));
 
-
 const origObjects = [
     { name: '3rd', value: 3 },
     { name: '1st', value: 1 },
@@ -25,22 +24,6 @@ const origObjects = [
 
 const bucket_name = 'simple objects p';
 const key = 'single object';
-
-// var opts = {
-//   bucket: 'test_bucket',
-//   key: 'test_key',
-//   indices: {
-//     test_index_one: '45'
-//     test_index_two: 'foo'
-//   },
-//   returnBody: true, // (optional) whether to return the contents of the stored object. defaults to false 
-//   value: 'test_value_here'
-// }
-// var promise = client.saveWithKey(opts)
-// promise.then(function() {
-//   // if key is not found value will be undefined 
-//   console.dir('key saved')
-// })
 
 client.saveWithKey({
     bucket: bucket_name,
