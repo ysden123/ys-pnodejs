85a9d169f8d49fcf34ad501e0c237e03249f950b
ysden123 <ysden123@gmail.com>
2016-06-16 21:08:36 +0300

    use semaphore with chunks

---
+++ modules/promises/chunks/useSemaphore.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const amount = 10000;
+// const amount = 353;
+// const amount = 1;
+const semaphore = require('semaphore')(1);
+
+let counter = 0;
+const step = 100;
+
+function job(i) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            ++counter;
+            resolve();
+        }, 100 + (i % 2) * 250);
+    });
+}
+
+function execute() {
+    return new Promise((resolve, reject) => {
+        for (let start = 1; start <= amount; start = start + step) {
+            semaphore.take(() => {
+                try {
+                    let tasks = [];
+                    for (let i = start; i <= start + step - 1 && i <= amount; ++i) {
+                        tasks.push(job(i));
+                    }
+
+                    Promise.all(tasks)
+                        .then(() => {
+                            try {
+                                if (start >= amount - step) {
+                                    console.log('resolving');
+                                    resolve();
+                                }
+                                semaphore.leave();
+                            }
+                            catch (err) {
+                                console.error(err);
+                                reject(err);
+                            }
+                        })
+                }
+                catch (err) {
+                    console.error(err);
+                    reject(err);
+                }
+            });
+        }
+    })
+}
+
+console.time('w1');
+execute()
+    .then(() => { 
+        console.log(`Done. counter=${counter}`);
+        console.timeEnd('w1');
+     })
+    .catch(err => console.error(err))
