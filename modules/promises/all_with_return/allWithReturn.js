aa2e647ab53789534ea93f3d6ecc1f1756fae25e
ysden123 <ysden123@gmail.com>
2016-07-16 13:37:26 +0300

    all with simple return and return Promise.resolve

---
+++ modules/promises/all_with_return/allWithReturn.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function p(i) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('val' + i);
+        }, 100 + Math.random() * 1000)
+    });
+}
+
+let a = [];
+for (let i = 1; i <= 10; ++i) {
+    a.push(
+        p(i)
+            .then(v => {
+                return { k: 'k' + i, v: v };
+            })
+    )
+}
+Promise.all(a)
+    .then(rslt => {
+        rslt.forEach(r => {
+            console.log(`k=${r.k}, v=${r.v}`);
+        })
+    })
