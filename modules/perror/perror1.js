6039a65769e261f2b532998a464704488690eda5
ysden123 <ysden123@gmail.com>
2017-06-09 19:43:51 +0300

    throw new Error vs throw

---
+++ modules/perror/perror1.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+'use strict';
+const util = require('util');
+
+try{
+    throw new Error("error text 1")
+}
+catch(err){
+    console.log(util.inspect(err,{ showHidden: true, depth: true }));
+}
+
+try{
+    throw "error text 2"
+}
+catch(err) {
+    console.log(util.inspect(err, {showHidden: true, depth: true}));
+}
